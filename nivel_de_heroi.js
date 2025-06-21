2178e57d5b0ed8ed8376d7f94215f8cd46d3f310
Rondinele Mamede <148896894+rondineley@users.noreply.github.com>
2025-06-21 17:02:57 -0300

    Add files via upload

---
+++ nivel_de_heroi.js
@@ -0,0 +1,32 @@
+function classificarHeroi() {
+  const nome = document.getElementById('nome').value.trim();
+  const xp = parseInt(document.getElementById('xp').value);
+  const resultado = document.getElementById('resultado');
+
+  if (nome === '' || isNaN(xp)) {
+    resultado.innerHTML = 'Por favor, preencha todos os campos corretamente.';
+    return;
+  }
+
+  let nivel = '';
+
+  if (xp < 1000) {
+    nivel = 'Ferro';
+  } else if (xp >= 1001 && xp <= 2000) {
+    nivel = 'Bronze';
+  } else if (xp >= 2001 && xp <= 5000) {
+    nivel = 'Prata';
+  } else if (xp >= 6001 && xp <= 7000) {
+    nivel = 'Ouro';
+  } else if (xp >= 7001 && xp <= 8000) {
+    nivel = 'Platina';
+  } else if (xp >= 8001 && xp <= 9000) {
+    nivel = 'Ascendente';
+  } else if (xp >= 9001 && xp <= 10000) {
+    nivel = 'Imortal';
+  } else if (xp >= 10001) {
+    nivel = 'Radiante';
+  }
+
+  resultado.innerHTML = `🦸‍♂️ O Herói de nome <strong>${nome}</strong> está no nível de <strong>${nivel}</strong>.`;
+}
